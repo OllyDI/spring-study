2c7885dda73365c4b56a3e1fe1d0def90a30adfb
OllyDI <choiseungho11@hanmail.net>
2024-03-04 16:27:29 +0900

    24.03.04

--- src/main/java/com/study/project/controller/HelloController.java
+++ src/main/java/com/study/project/controller/HelloController.java
@@ -3,13 +3,23 @@ package com.study.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
+    // GetMapping은 주소값 ex) localhost:8080/hello
+    // return은 html이름 ex) hello.html
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    // localhost:8080/hello-mvc?name="파라미터값" 이런식으로 파라미터 전달가능
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
